baddd7799992af7c682db0cd153f47b217390fa8
ryotaro-ueda <charcoalueda0518@gmail.com>
2022-07-11 20:14:23 +0900

    サイコロを四つ振るスクリプトを追加

--- Assets/Ueda/Script/Dice.cs
+++ Assets/Ueda/Script/Dice.cs
@@ -7,7 +7,7 @@ public class Dice : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        RollDice();
     }
 
     // Update is called once per frame
@@ -20,8 +20,34 @@ public class Dice : MonoBehaviour
         foreach(Transform a in _diceTrans)
         {
             int diceNum = Random.Range (1,7) ;
-            GameObject dice = Instantiate( _dicePrefab,a,a);
+            GameObject dice = Instantiate( _dicePrefab,a);
             Animator anim = dice.GetComponent<Animator>();
+            switch(diceNum)
+            {
+                case 1:
+                    anim.SetTrigger("No1");
+                    break;
+
+                case 2:
+                    anim.SetTrigger("No2");
+                    break;
+
+                case 3:
+                    anim.SetTrigger("No3");
+                    break;
+
+                case 4:
+                    anim.SetTrigger("No4");
+                    break;
+
+                case 5:
+                    anim.SetTrigger("No5");
+                    break;
+
+                case 6:
+                    anim.SetTrigger("No6");
+                    break;
+            }
         }
                 
 
