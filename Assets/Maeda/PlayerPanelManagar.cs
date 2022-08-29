b7234357cf5c5134d89231e3fa61b4125ac8a047
Maety <maetyuryu@gmail.com>
2022-08-29 19:40:21 +0900

    資金と持ち株の変動を受け取る関数を作成

--- Assets/Maeda/PlayerPanelManagar.cs
+++ Assets/Maeda/PlayerPanelManagar.cs
@@ -5,6 +5,21 @@ using UnityEngine.UI;
 
 public class PlayerPanelManagar : MonoBehaviour
 {
-    [SerializeField, Tooltip("‰æ–Êã‚É•\¦‚·‚éŠ‹à‚ÌText")]
-    Text _moneyText = default;
+    [SerializeField, Tooltip("‰æ–Êã‚É•\¦‚·‚é‘‹à‚ÌText")]
+    Text _fundText = default;
+
+    [SerializeField, Tooltip("‰æ–Êã‚Éo‚é4í‚ÌŠ”‰¿‚ÌText")]
+    Text[] _stockTypes;
+
+    /// <summary>
+    /// ‘‹à‚Æ‚¿Š”‚Ì•Ï“®‚ğó‚¯‚Æ‚é
+    /// </summary>
+    /// <param name="money"></param>
+    /// <param name="stockType"></param>
+    /// <param name="stockIndex"></param>
+    public void MoneyAndStockChange(int money , int stockType , int stockIndex) 
+    {
+        _fundText.text = money.ToString();
+        _stockTypes[stockType].text = stockIndex.ToString();
+    }
 }
