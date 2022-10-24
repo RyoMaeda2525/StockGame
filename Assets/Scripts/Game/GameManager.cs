a742ddc617906c27b918379b363105ce9089d63c
st37071556@icloud.com <st37071556@icloud.com>
2022-10-24 11:45:36 +0900

    InputFieldで相手のプレイヤー番号を指定し、Battleの判定を行う予定

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -122,6 +122,7 @@ public class GameManager : MonoBehaviour, IPunTurnManagerCallbacks
         print($"player{playerIndex+1}は、player{stockIndex+1}の株を{stock[0]}個買った。");
         PlayerUIManager.instance.BuyStockChange(playerIndex,stockIndex,stock[0]);
     }
+
     /// <summary>
     /// 戦って株が減る
     /// </summary>
@@ -131,7 +132,7 @@ public class GameManager : MonoBehaviour, IPunTurnManagerCallbacks
     void BattleStock(int targetIndex, int playerIndex, int[] dise)
     {//戸澤 担当予定
         //ターン終了時まで非同期処理で待つ アシンクアウェイト
-        ////
+        //ボタンで呼んだり、相手を選ぶ機能を作る
         StartCoroutine(WaitForEndOfTurns());//コルーチン開始 このタイミングでええんか？
         
         if (dise[0] + dise[1] > dise[2] + dise[3])
@@ -201,7 +202,7 @@ public class GameManager : MonoBehaviour, IPunTurnManagerCallbacks
     /// </summary>
     /// <param name="targetIndex">戦う相手</param>
     public void Battle(int targetIndex)
-    {
+    {//戸澤 担当予定
         int[] dise = new int[4];
         for(int i = 0; i > dise.Length; i++)
         {
